216fb81b41383c7ba10c0350a6ce506b0edda936
Khushvind <bruce_wayne_>
2024-01-12 21:55:00 +0530

    Add solution for problem `F - Bouncy Ball` Link: https://codeforces.com/contest/1807/problem/F Tags: brute force, dfs and similar, implementation, *1700 Ref: https://codeforces.com/contest/1807/submission/241308329

---
+++ codeforces/1807/F.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+#define FOR(a,b) for(long long i = a; i < b; i++)
+
+int solve(){
+    int n, m, x1, y1, x2, y2;
+    string d_string;
+    cin >> n >> m >> x1 >> y1 >> x2 >> y2;
+    x1--;x2--;y1--;y2--;
+    cin >> d_string;
+    int d = (d_string[0] == 'U' ? 1+(d_string[1] == 'R' ? 2 : 0) : 0+(d_string[1] == 'R' ? 2 : 0));
+    bool vis[n][m][4];
+    memset(vis, false, sizeof(vis));
+    int i = x1, j = y1;
+    int bounces = 0;
+    while(!vis[i][j][d]){
+        if(i == x2 && j == y2){
+            cout << bounces << endl; 
+            return 0;
+        }
+        int na = 0;
+        if(d%2 == 1 && i == 0){d-=1;na++;}
+        if(d%2 == 0 && i == n-1){d+=1;na++;}
+        if(d >= 2 && j == m-1){d-=2;na++;}
+        if(d < 2 && j == 0){d+=2;na++;}
+        bounces+=min(1, na);
+        if(vis[i][j][d]){
+            break;
+        }
+        vis[i][j][d] = true;
+        if(d%2 == 1){i--;}else{i++;}
+        if(d >= 2){j++;}else{j--;}
+    }
+    cout << -1 << endl;
+    return 0;
+}
+
+int main(void){
+    int times;
+    cin >> times;
+    while (times--) {
+        solve();
+    };
+    return 0;
+}
