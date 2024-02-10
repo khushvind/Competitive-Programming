0dd879bad437e85a1c60af8790e11f95352306b2
Khushvind <bruce_wayne_>
2024-02-10 15:48:00 +0530

    Add solution for problem `C - Choose the Different Ones!` Link: https://codeforces.com/contest/1927/problem/C Tags: brute force, greedy, math, *1000 Ref: https://codeforces.com/contest/1927/submission/245700065

---
+++ codeforces/1927/C.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+#define FOR(a,b) for(long long i = a; i < b; i++)
+
+int solve(){
+    int n,m,k; cin >> n >> m >> k;
+    vector<int> a(n),b(m);
+    for (int i=0; i<n; i++) cin >> a[i];
+    for (int i=0; i<m; i++) cin >> b[i];
+    set<int> st1,st2;
+    for (int i = 0 ; i < a.size() ; i++){
+        if (a[i] <= k) st1.insert(a[i]);
+    }
+    for (int i = 0 ; i < b.size(); i++){
+        if (b[i] <= k) st2.insert(b[i]);
+    }
+
+    int x = 0, y =0;
+
+    for (int i = 1 ; i <= k; i++){
+        if (st1.count(i) && !st2.count(i)){
+            x++;
+        } else if (!st1.count(i) && st2.count(i)){
+            y++;
+        } else if (st1.count(i) && st2.count(i)){
+            continue;
+        } else {
+            cout << "NO" << endl;
+            return 0;
+        }
+        if (x > k/2 || y > k/2) {
+            cout << "NO" << endl;
+            return 0;
+        }
+    }
+    cout << "YES" << endl;
+    
+    
+
+    
+
+    return 0;
+}
+
+int main(void){
+    int times;
+    cin >> times;
+    while (times--) {
+        solve();
+    };
+    return 0;
+}
