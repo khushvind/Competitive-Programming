7717e57afaed50dd7a434a4ff69a5aab4299db45
Khushvind <bruce_wayne_>
2024-03-01 18:36:00 +0530

    Add solution for problem `C - Serval and Toxel's Arrays` Link: https://codeforces.com/contest/1789/problem/C Tags: combinatorics, dp, implementation, math, *1500 Ref: https://codeforces.com/contest/1789/submission/249087153

--- codeforces/1789/C.cpp
+++ codeforces/1789/C.cpp
@@ -1,44 +1,34 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-using ll = long long;
-#define FOR(a,b) for(long long i = a; i < b; i++)
+typedef long long ll;
 
-int solve(){
-    ll n,m;cin >> n >> m;
-    vector<ll> a(n);
-    vector<ll> occur(m+n+1,-1),cnt(n+m+1);
-    for (int i = 0; i < n ;i++) {
-        cin >> a[i];
-        occur[a[i]] = 0;
-    }
-    for (int j = 0; j < m; j++){
-        int p,v; cin >> p >> v;
-        p--;
-        cnt[a[p]]+= j+1-occur[a[p]];
-        occur[a[p]] = -1;
-        occur[v] = j+1;
-        a[p] = v;
-    }
-    for (int i = 1; i <= m+n; i++){
-        int x = occur[i];
-        if (x != -1){
-            cnt[i]+= m+1-x;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n,m;
+        cin >> n >> m;
+        vector<int> c(n+m+1,0);
+        vector<int> a(n+1,0);
+        for(int i=1;i<=n;i++)
+        {
+            cin >> a[i];
+            c[a[i]]+=(m+1);
         }
+        for(int i=1;i<=m;i++)
+        {
+            int p,b;
+            cin >> p >> b;
+            c[a[p]]-=(m-i+1);
+            c[b]+=(m-i+1);
+            a[p]=b;
+        }
+        ll res=(ll(m)*(m+1)/2*ll(2*n));
+        for(int i=1;i<=n+m;i++) res-=(ll(c[i])*(c[i]-1)/2);
+        cout << res << "\n";
     }
-    ll sum = 0;
-    for (int i = 1; i <= m+n; i++) {
-        sum += (m*(m+1))/2 - ((m-cnt[i])*(m-cnt[i]+1))/2;
-    }
-    cout << sum << endl;
-    return 0;
-}
-
-int main(void){
-    int times;
-    cin >> times;
-    while (times--) {
-        solve();
-    };
     return 0;
 }
